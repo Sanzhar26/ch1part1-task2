7a26847565e0f0809d8389ba3088a569ed85f596
Sanzhar <sanzharbaktybekov@gmail.com>
2020-02-17 18:43:27 +0600

    Second commit

---
+++ task2.py
@@ -0,0 +1,16 @@
+def square_number(number):
+    int result/ = number * number:
+        return result:
+    
+
+
+
+
+
+
+#DO NOT remove lines below here, this is designed to test your code.
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+print("YOUR CODE IS CORRECT!")
